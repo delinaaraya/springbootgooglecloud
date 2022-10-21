2b5e7ea4f384465fdf3bc1017856f0f5d29509c7
delinaaraya <91509315+delinaaraya@users.noreply.github.com>
2022-10-20 22:10:17 -0700

    Add files via upload

--- src/main/java/com/gcu/model/ProductModel.java
+++ src/main/java/com/gcu/model/ProductModel.java
@@ -1,17 +1,23 @@
 package com.gcu.model;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class ProductModel {
 	//The properties for the product.
+	@Min(value=1, message="Id must be 1 character")
 	private int id;
 	
+	@Size(min=1, max=32, message="Product name must be between 1 and 32 characters")
 	@NotNull(message="Product name is a required field")
 	private String name;
 
+	@Min(value=1, message="Price must be 1 character")
 	@NotNull(message="Price is a required field")
 	private float price;
 	
+	@Size(min=1, max=32, message="Description must be between 1 and 32 characters")
 	@NotNull(message="Description is a required field")
 	private String description;
 	
